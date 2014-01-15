43de9d3765e986317c3642dbe7b0ec1263bd8b81
mkuzak <mateusz.kuzak@gmail.com>
2014-01-15 17:11:38 +0100

    Added coverage plotting and tests for alignment and coverage plotting.

--- R/vis.r
+++ R/vis.r
@@ -1,4 +1,5 @@
 library(ggbio)
+
 #' Plot aligned reads over annotated region
 #' @param aln read alignments in GAlignments objet (GenomicTRanges)
 #' @param annot genome annotations in GRanges object
@@ -9,6 +10,23 @@ PlotAlignAnnot <- function(aln, annots, plotfile) {
     png(filename=plotfile,
         width=1024, height=512)  
     print(tracks(alignments=reads.track,
+                 annotation=annots.track,
+                 heights=c(5,1)))  
+    dev.off()
+}
+
+#' Plot aligned reads over annotated region
+#' @param aln read alignments in GAlignments objet (GenomicTRanges)
+#' @param annot genome annotations in GRanges object
+PlotCoverageAnnot <- function(aln, annots, plotfile) {
+    cov <- coverage(aln)
+    coverage.track <- autoplot(cov, binwidth=10)
+    
+    annots.track <- autoplot(annots,
+                             aes(color=group, fill=group))
+    png(filename=plotfile,
+        width=1024, height=512)  
+    print(tracks(coverage=coverage.track,
                  annotation=annots.track,
                  heights=c(5,1)))  
     dev.off()
---
+++ tests/testthat/test_vis.r
@@ -0,0 +1,53 @@
+library(rtracklayer)
+library(GenomicRanges)
+context("vis")
+
+# location of sample data for testing
+dataDir <- "../data"
+consensusSeqId <- "EMBOSS_001|consensus"
+
+cleanup <- function() {
+  files <- list.files(file.path(dataDir))
+  file.pattern <- '^test_'
+  test.files <- files[sapply(files,
+                             function(x){str_detect(x, file.pattern)})]
+  test.file.paths <- sapply(test.files,
+                            function(x) {file.path(dataDir, x)})
+  file.remove(test.file.paths)
+}
+
+test_that("Plotting functions in vis generate and saves a plots",{
+  # input files
+  gfffile <- file.path(dataDir, "rRNA.gff")
+  bamfile <- file.path(dataDir, "sample.bam")
+  
+  # import data
+  ## genome annotations
+  gff <- import(gfffile, asRangedData=F)
+  ## aligned reads
+  gr <- GRanges(seqnames = c("EMBOSS_001|consensus"),
+                ranges = IRanges(1, width=7000),
+                strand = "*"
+  )
+  param <- ScanBamParam(which=gr)
+  consensus.aln <- readGAlignments(bamfile, param=param)
+  
+  # get rid of unused selevels
+  seqlevels(consensus.aln) <- cons.id
+  
+  ## fetch only consensus sequence from gff
+  consensus.annots <- gff[seqnames(gff)==consensusSeqId]
+  plotfile <- file.path(dataDir, 'test_alnannot.png')
+  
+  # test PlotAlignAnnot function
+  plotfile1 <- file.path(dataDir, 'test_alnannot.png')
+  PlotAlignAnnot(consensus.aln, consensus.annots, plotfile1)
+  expect_true(file.exists(plotfile1))
+  
+  # test PlotCoverageAnnot
+  plotfile2 <- file.path(dataDir, 'test_covannot.png')
+  PlotCoverageAnnot(consensus.aln, consensus.annots, plotfile2)  
+  expect_true(file.exists(plotfile2))
+})
+
+cleanup()
