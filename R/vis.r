af37388bb2e552942041c1db2aa023d345bb6138
mkuzak <mateusz.kuzak@gmail.com>
2014-02-05 16:59:20 +0100

    Added per cycle quality plotting.

--- R/vis.r
+++ R/vis.r
@@ -1,4 +1,6 @@
+library(ShortRead)
 library(ggbio)
+library(ggplot2)
 
 #' Plot aligned reads over annotated region.
 #' @param aln read alignments in GAlignments objet (GenomicTRanges)
@@ -30,4 +32,21 @@ PlotCoverageAnnot <- function(aln, annots, plotfile) {
                  annotation=annots.track,
                  heights=c(5,1)))  
     dev.off()
+}
+
+#' Plot qaality scores per cycle
+#' @param data.dir string path to data
+#' @param file.pattern string pattern for input files
+#' @param type stirng input type, 'fastq' is default value
+#' @return plot 
+PlotPerCycleQualityScores <- function(data.dir, file.pattern, type="fastq") {
+    max.rlen <- 200
+    sreadq.qa <- qa(data.dir, file.pattern, type=type)
+    perCycle <- sreadq.qa[["perCycle"]]
+    perCycle.q <- perCycle$quality
+    perCycle.q <- perCycle.q[perCycle.q$Cycle <= max.rlen, ]
+    pcq <- perCycle.q[rep(seq_len(nrow(perCycle.q)), perCycle.q$Count), ]
+    p <- ggplot(pcq, aes(factor(Cycle), Score))
+    p <- p + geom_boxplot()
+    return(p)
 }
