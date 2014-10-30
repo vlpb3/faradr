56b0ab648301c07ba6c57ac418aba8cbb47743f7
mkuzak <mateusz.kuzak@gmail.com>
2014-10-30 14:05:48 +0100

    Implement normalised spike count plot

--- R/spikes.r
+++ R/spikes.r
@@ -1,5 +1,6 @@
 #' Plot spike counts
 #' @param counts_file csv file with spike counts
+#' @importFrom ggplot2 ggplot geom_point geom_line theme
 #' @export
 PlotSpikes <- function(counts_file) {
   counts <- read.csv(counts_file, sep="/t")
@@ -15,15 +16,30 @@ PlotSpikes <- function(counts_file) {
 #' Plot normalised spike counts
 #' @param counts_file csv file with spike counts
 #' @param sample_read_couts named list, number of reads per each sample
+#' @importFrom ggplot2 ggplot geom_point geom_line theme
+#' @importFrom reshape2 melt
+#' @importFrom dplyr %>% group_by transform
 #' @export
 PlotNormSpikes <- function(counts_file, total_reads) {
+  # load count tables into dataframes
   counts <- read.csv(counts_file, sep="\t")
-  total_reads_df <- read.csv(total_reads, sep="\t")
-  samples <- names(counts)[2:length(counts)]
+  colnames <- c("sample", "total")
+  total_reads_df <- read.csv(total_reads, header=F, col.names=colnames, sep="\t")
+  
+  samples <- names(counts)[2:length(counts)]  
+
   molten_counts <- melt(counts)
   names(molten_counts) <- c("seqid", "sample", "count")
+  molten_counts$total <- rep(0, nrow(molten_counts))
+  for (s in samples){
+    total <- total_reads_df[total_reads_df$sample == s,]$total
+    molten_counts[molten_counts$sample == s,]$total <- total
+  }
+    
+  # normalize per milion of reads (total)
   norm_counts <- molten_counts %>% group_by(sample) %>%
-    transform(count_pm=count/sum(count)*0.001)
+    transform(count_pm=(count*1e6)/total)
+
   p <- ggplot(norm_counts, aes(seqid, count_pm, group=sample, colour=sample))
   p <- p + geom_point()
   p <- p + geom_line()
